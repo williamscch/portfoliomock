b6c022798cc9b59a2759d76ff384df3d7dadfded
wdavidcch <david.13.2121@gmail.com>
2022-06-14 13:40:01 -0400

    Add ESlint

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
-const button = document.querySelector(".menu");
-const menu = document.querySelector(".desktop");
-const body = document.querySelector("body");
+const button = document.querySelector('.menu');
+const menu = document.querySelector('.desktop');
+const body = document.querySelector('body');
+const close = document.querySelector('.x');
+const closeLinks = document.querySelector('.link', '.link2', '.link3');
 
-button.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  body.classList.toggle("active");
+button.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  body.classList.toggle('active');
+});
+
+close.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  body.classList.toggle('active');
+});
+
+closeLinks.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  body.classList.toggle('active');
 });
