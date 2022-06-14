1d77fbe6948f130c278860d660349983636ca8c3
wdavidcch <david.13.2121@gmail.com>
2022-06-14 13:44:41 -0400

    Add eventlistener to mobile-menu li

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@ const button = document.querySelector('.menu');
 const menu = document.querySelector('.desktop');
 const body = document.querySelector('body');
 const close = document.querySelector('.x');
-const closeLinks = document.querySelector('.link', '.link2', '.link3');
+const closePortfolio = document.querySelector('.link');
+const closeAbout = document.querySelector('.link2');
+const closeContact = document.querySelector('.link3');
 
 button.addEventListener('click', () => {
   menu.classList.toggle('active');
@@ -14,7 +16,17 @@ close.addEventListener('click', () => {
   body.classList.toggle('active');
 });
 
-closeLinks.addEventListener('click', () => {
+closePortfolio.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  body.classList.toggle('active');
+});
+
+closeAbout.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  body.classList.toggle('active');
+});
+
+closeContact.addEventListener('click', () => {
   menu.classList.toggle('active');
   body.classList.toggle('active');
 });
